ea1832bc3139b6196d6d3cfc8f35bd7149737ef1
jroisgit config --global user.name jroi <jrois_r@live.nl>
2021-03-27 17:59:48 +0100

    current time to float value based on position in wake interval

--- test/time.py
+++ test/time.py
@@ -1,20 +1,36 @@
-from time import localtime, strftime
-from gpiozero import PWMLED
-from gpiozero.pins.pigpio import PiGPIOFactory
-import RPi.GPIO as GPIO
-from time import sleep
-from datetime import datetime, timedelta, time
-
-#set up pwm leds
-factory = PiGPIOFactory()
-warm = PWMLED(14, pin_factory=factory)
+from datetime import datetime, time, timedelta
+from scipy.interpolate import interp1d
 
 # time light
-t = datetime.now()
-wake_up_time = time(18, 00)
-dt = timedelta(minutes=32)
+def wake_light_value(wt, dt):
+    """
+    return float based on how far in the wake interval the current time is
+    :param wt: wake time [hour, minute]
+    :param dt: time period in minutes
+    :return:
+    """
+    [h, m] = wt
+    wake_time = datetime(1, 1, 1, hour=h, minute=m)
+    timeperiod = timedelta(minutes=dt)
+    current_time = datetime.now()
+    if (wake_time - timeperiod).time() < current_time.time() < wake_time.time():
+        return get_light_value(wt, dt)
+    else:
+        return False
 
-print(t == wake_up_time - dt)
-# if H == wake_up_time[0] and M > wake_up_time[1]-30:
+def time2integer(time):
+    return 60*time.hour + time.minute
 
 
+def get_light_value(wt, dt):
+    current_time = datetime.now()
+    [h, m] = wt
+    wake_time = datetime(1, 1, 1, hour=h, minute=m)
+    timeperiod = timedelta(minutes=dt)
+    f = interp1d([time2integer((wake_time-timeperiod).time()), time2integer(wake_time)],[0, 1])
+    return f(time2integer(current_time))
+
+# dt = 33
+# wt = [18, 00]
+#
+# print(wake_light_value(wt, dt))
