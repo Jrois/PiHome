6e8a54a0cdcf21a4becba98e5e5783c335736e69
JroisPi <Jrois_R@live.nl>
2021-04-14 22:33:04 +0200

    oo on/off fixed

--- ledstrip_control.py
+++ ledstrip_control.py
@@ -48,8 +48,11 @@ def on_off(ledstrip, event):
         event.wait()
         if not ledstrip.on:
             ledstrip.set_strip(ledstrip.state())
+            ledstrip.on = True
         else:
             ledstrip.all_off()
+            ledstrip.on = False
+        time.sleep(0.5)
         event.clear()
 
 
@@ -65,8 +68,6 @@ dimDownThread.start()
 IOThread.start()
 
 ledstrip.all_off()
-ledstrip.set_strip(ledstrip.state())
-print(ledstrip.state(), ledstrip.val)
 while True:
     if buttons == [1, 0, 0, 0]:
         dimUpEvent.set()
@@ -74,42 +75,3 @@ while True:
         dimDownEvent.set()
     if buttons == [1, 1, 0, 0]:
         IOEvent.set()
-
-# s = State()
-# val = 0.5
-# all_off()
-# wake_up_time = [7, 30]
-# while True:
-#     # switch between states
-#     if on and buttons_pressed()==[0,0,1,0]:
-#         s.up()
-#         all_off()
-#         set_strip(s.state(),val)
-#         sleep(0.5)
-#     if on and buttons_pressed()==[0,0,0,1]:
-#         s.down()
-#         all_off()
-#         set_strip(s.state(),val)
-#         sleep(0.5)
-#
-#     # turn on/off
-#     if not on and buttons_pressed()==[1,1,0,0]:
-#         on = True
-#         set_strip(s.state(),val)
-#         sleep(0.5)
-#     if on and buttons_pressed()==[1,1,0,0]:
-#         on = False
-#         set_strip(s.state(),0)
-#         sleep(0.5)
-#
-#     # dimming
-#     if on and buttons_pressed()==[1,0,0,0]:
-#         if val < 0.9:
-#             val += 0.05
-#         set_strip(s.state(),val)
-#         sleep(0.25)
-#     if on and buttons_pressed()==[0,1,0,0]:
-#         if val > 0.1:
-#             val -= 0.05
-#         set_strip(s.state(),val)
-#         sleep(0.25)
